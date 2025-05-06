555dddc208ba4516dc152abdf3eaf9d05ac1b96b
dennissammy77 <dennissammy77@gmail.com>
2025-05-06 09:47:35 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f'Hello, {name}!')
     pass
 
 def greet_with_default(name="programmer"):
+    print(f'Hello, {name}!')
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
-    pass
+    if type(number) is int:
+        return number / 2
+    elif type(number) is float:
+        return number / 2
+    else:
+        return None
